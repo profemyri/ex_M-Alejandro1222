827c30b04b1dbbff52f42ef67b1751b2df2d3883
Alejandro1222 <127210775+Alejandro1222@users.noreply.github.com>
2025-03-21 13:38:03 +0100

    asd

--- ex_html-css-js.js
+++ ex_html-css-js.js
@@ -1,1 +1,11 @@
-//Escribe aquí tu código JavaScript
+document.getElementById("formulario").onsubmit=function(value){
+    let validador=document.getElementById("nombre").trim;
+    if(validador=""){
+        value.preventDefault;
+    }else{
+        validador="";
+        if()
+    }
+
+}
+
