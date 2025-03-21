d0a861512fc42cd44bf4b9cf523cbda1dffe0250
Alejandro1222 <127210775+Alejandro1222@users.noreply.github.com>
2025-03-21 14:20:48 +0100

    asd

--- ex_html-css-js.js
+++ ex_html-css-js.js
@@ -2,19 +2,43 @@ document.getElementById("formulario").onsubmit=function(event){
     let validador=document.getElementById("nombre").trim;
     let validadorEmail=document.getElementById("email");
     let direccion=document.getElementById("direccion");
+    let numero=document.getElementById("numero");
+    let error=document.getElementById("error");
+    
     if(validador===""){
         event.preventDefault;
     }else{
         validador="";
         if(!validadorEmail.contains("@")){
             event.preventDefault;
+            error.innerHTML="Algunos campos son incorrectos o están incompletos";
         }else{
             validadorEmail="";
             if(direccion.length<18){
                 event.preventDefault;
+                error.innerHTML="Algunos campos son incorrectos o están incompletos";
+            }else{
+                direccion="";
+                if(numero!==9){
+                    event.preventDefault;
+                    error.innerHTML="Algunos campos son incorrectos o están incompletos";
+                }else{
+                    numero="";
+                }
             }
         }
     }
-
+    if(!confirm("¿Seguro que quieres confirmar y enviar tu pedido ahora?")){
+        event.preventDefault;
+    }
 }
+getElementById("instrucciones").oninput=function(event){
+    let numCaracteres = getElementById("instrucciones");
+    let contador = getElementById("contador");
+    contador=numCaracteres.value.length;
+    if(numCaracteres.value.length>150){
+        event.preventDefault;
+    }
+};
+if(instrucciones)
 
